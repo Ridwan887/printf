4bf2f360c99c9abab584ceb90f86fbe5a6de1ca6
Ncossa29 <nyeleti.strela@gmail.com>
2023-06-27 02:32:26 -0700

    flags

--- get_flags.c
+++ get_flags.c
@@ -8,8 +8,7 @@
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
+
 	int h, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
